7dca22779d1774ac4652c5b4a975ad189c805ccf
Adrian Yi <sjay87@gmail.com>
2018-10-10 12:19:41 -0700

    Change to /logs

--- resource_monitor.py
+++ resource_monitor.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.call("while true; do nvidia-smi >> /data/resources.log; sleep 10; done")
+subprocess.call("while true; do nvidia-smi >> /logs/resources.log; sleep 10; done")
 subprocess.call("python mnist.py --num_steps 5000", shell=True)
