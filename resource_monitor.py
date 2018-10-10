dee1df678bc7b530e25269413f0843d990cf11f4
Adrian Yi <sjay87@gmail.com>
2018-10-10 13:01:42 -0700

    Terminate monitoring when python script ends

--- resource_monitor.py
+++ resource_monitor.py
@@ -1,4 +1,9 @@
+import os
+import signal
 import subprocess
 
-subprocess.Popen("while true; do nvidia-smi >> /logs/resources.log; sleep 10; done", shell=True)
+# subprocess.Popen("while true; do nvidia-smi >> /logs/resources.log; sleep 10; done", shell=True)
+p = subprocess.Popen("while true; do echo 'abc-test' >> resources.log; sleep 5; done", shell=True)
 subprocess.call("python mnist.py --num_steps 5000", shell=True)
+
+os.killpg(os.getpgid(p.pid), signal.SIGTERM)
